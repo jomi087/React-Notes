e887808970cf1744ab9d0f3aa64a6fd779704918
jomi087 <jomi>
2025-04-09 23:15:38 +0530

    Refactor main.jsx for improved readability

--- 4-Implimentation of JSX/src/main.jsx
+++ 4-Implimentation of JSX/src/main.jsx
@@ -1,14 +1,17 @@
 //!simple and basic implimentation of JSX in react
-import  ReactDOM  from 'react-dom/client'  
+import ReactDOM from 'react-dom/client'  
+import React from 'react'
 
-let heading = (<h1>hlo world 1</h1>) ; 
+// const heading = React.createElement("h1", {}, "hlo world 1");    instead of writting like this we will write in a jsx format  given below
+
+let heading = (<h1>hlo world 1</h1>);  // this is jsx *(doubts read readMe)
+
 let root = ReactDOM.createRoot(document.getElementById('root')) 
 root.render(heading)
 
-
-
-
 //! vite has its own way which is given below 
+//there is not much diffrence  from the abouve code only they use some destructurng + jsx + code continuation by using "." & a strict mode (which is only for development mode)
+
 /*
   import { StrictMode } from 'react'             //warns about these deprecated methods
   import { createRoot } from 'react-dom/client'  //React 18 has no default export that's why we have to use destructuring 
